4b908abbedbe66e3f6f415f255fb6021db2fc201
Shakthi <shakthi.jashwanth@gmail.com>
2026-04-29 10:53:25 +0530

    testing jenkins

--- backend/app/main.py
+++ backend/app/main.py
@@ -134,7 +134,7 @@ def health():
 
 @app.get("/")
 def root():
-    return {"message": "CI/CD working 🚀"}
+    return {"message": "CI/CD pipeline is working"}
 
 # -------------------- QUERY --------------------
 @app.post("/query", response_model=QueryResponse)
